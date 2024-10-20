02013611058383bf868e811bc401918346d49e8e
wolfiee42 <saifalislam2022@gmail.com>
2024-10-20 17:11:00 +0600

    feat: seatting up jest along with matchers like toBe() and toEqual().

---
+++ jest-course-freeCodeCamp/sum.test.js
@@ -0,0 +1,16 @@
+const sum = require("./sum");
+
+test("adds 1 + 2 equals to 3.", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("two ples two is four", () => {
+  expect(2 + 2).toBe(4); // toBe is used for premivite data.
+});
+
+test("object assignment", () => {
+  const data = { one: 1 };
+  data["two"] = 2;
+
+  expect(data).toEqual({ one: 1, two: 2 }); //toEqual is used for non-premitive data.
+});
