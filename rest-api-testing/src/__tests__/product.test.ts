3a4fee8c4e6605c82c66bc0606b3a052a27a3bca
wolfiee42 <saifalislam2022@gmail.com>
2024-10-28 12:34:57 +0600

    creating testcase for product existance and the product itself.

--- rest-api-testing/src/__tests__/product.test.ts
+++ rest-api-testing/src/__tests__/product.test.ts
@@ -2,9 +2,21 @@ import supertest from "supertest";
 import createServer from "../utils/server";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import { createProduct } from "../service/product.service";
 
 
 const app = createServer();
+
+const userId = new mongoose.Types.ObjectId().toString();
+
+export const productPayload = {
+  user: userId,
+  title: "Stylish Backpack",
+  description: "A versatile and trendy backpack perfect for school or travel.",
+  price: 49.99,
+  image: "https://example.com/images/backpack.jpg"
+}
+
 
 describe("product", () => {
 
@@ -18,12 +30,25 @@ describe("product", () => {
     await mongoose.connection.close();
   })
 
-
+  // if the product doesn't exist
   describe("get product route", () => {
     describe("given the product does not exist", () => {
       it('should return 404 error', async () => {
-        const productID = "product-123";
-        await supertest(app).get(`/api/products/${productID}`).expect(404)
+        const productId = "product-123";
+        await supertest(app).get(`/api/products/${productId}`).expect(404)
+      });
+    })
+  })
+
+
+  // if the product exist.
+  describe("get product route", () => {
+    describe("given the product exist", () => {
+      it('should return 200 and the product itself.', async () => {
+        const product = await createProduct(productPayload);
+        const { body, statusCode } = await supertest(app).get(`/api/products/${product.productId}`);
+        expect(statusCode).toBe(200);
+        expect(body.productId).toBe(product.productId);
       });
     })
   })
