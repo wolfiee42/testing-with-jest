00b9a7bd058bbc9ed501ce00b40e0a2f39e3788e
wolfiee42 <saifalislam2022@gmail.com>
2024-10-28 11:10:43 +0600

    feat: understanding MongoMemoryServer, beforeAll, afterAll methods of jest

--- rest-api-testing/src/__tests__/product.test.ts
+++ rest-api-testing/src/__tests__/product.test.ts
@@ -1,7 +1,24 @@
 import supertest from "supertest";
-import { app } from '../app'
+import createServer from "../utils/server";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+
+const app = createServer();
 
 describe("product", () => {
+
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri())
+  })
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+  })
+
+
   describe("get product route", () => {
     describe("given the product does not exist", () => {
       it('should return 404 error', async () => {
