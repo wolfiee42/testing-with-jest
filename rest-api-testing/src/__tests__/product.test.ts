38fc688eaebbbfd96176ae8f14c3d7195ef74fe1
wolfiee42 <saifalislam2022@gmail.com>
2024-10-28 13:13:30 +0600

    feat: understanding how an api call works with jest and supertest.

--- rest-api-testing/src/__tests__/product.test.ts
+++ rest-api-testing/src/__tests__/product.test.ts
@@ -3,6 +3,7 @@ import createServer from "../utils/server";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { createProduct } from "../service/product.service";
+import { signJwt } from "../utils/jwt.utils";
 
 
 const app = createServer();
@@ -12,9 +13,15 @@ const userId = new mongoose.Types.ObjectId().toString();
 export const productPayload = {
   user: userId,
   title: "Stylish Backpack",
-  description: "A versatile and trendy backpack perfect for school or travel.",
+  description: "A versatile and trendy backpack perfect for school or travel. A versatile and trendy backpack perfect for school or travel. A versatile and trendy backpack perfect for school or travel.",
   price: 49.99,
   image: "https://example.com/images/backpack.jpg"
+}
+
+export const userPayload = {
+  _id: userId,
+  email: 'jane.doe@example.com',
+  name: 'Jane Doe',
 }
 
 
@@ -51,5 +58,37 @@ describe("product", () => {
         expect(body.productId).toBe(product.productId);
       });
     })
+  })
+
+  describe("given the user is not logged in", () => {
+    it('should return a 403', async () => {
+      const { statusCode } = await supertest(app).post('/api/products');
+      expect(statusCode).toBe(403);
+    });
+  })
+
+  describe("given the user is logged in", () => {
+    it('should return a 200 and create the product.', async () => {
+      const jwt = signJwt(userPayload);
+
+      const { statusCode, body } = await supertest(app)
+        .post('/api/products')
+        .set('Authorization', `Bearer ${jwt}`)
+        .send(productPayload)
+
+      expect(statusCode).toBe(200)
+      expect(body).toEqual({
+        "__v": 0,
+        "_id": expect.any(String),
+        "createdAt": expect.any(String),
+        "description": "A versatile and trendy backpack perfect for school or travel. A versatile and trendy backpack perfect for school or travel. A versatile and trendy backpack perfect for school or travel.",
+        "image": "https://example.com/images/backpack.jpg",
+        "price": 49.99,
+        "productId": expect.any(String),
+        "title": "Stylish Backpack",
+        "updatedAt": expect.any(String),
+        "user": expect.any(String),
+      })
+    });
   })
 })
