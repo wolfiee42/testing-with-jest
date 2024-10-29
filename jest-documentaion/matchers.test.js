b7a83a0b4da6fe50989fe6412806759aac4d1a34
wolfiee42 <saifalislam2022@gmail.com>
2024-10-29 18:22:36 +0600

    test: learning .toBeCloseTo, .toHaveBeenCalled matchers.

--- jest-documentaion/matchers.test.js
+++ jest-documentaion/matchers.test.js
@@ -3,6 +3,8 @@
 const can = {
   name: "pamplemousse",
   ounces: 12,
+  stockInWarehouse: 0.1,
+  stockInStore: 0.2,
 };
 
 describe("The can", () => {
@@ -17,5 +19,31 @@ describe("The can", () => {
   //.not
   test("has a sopisticated name.", () => {
     expect(can.name).not.toBe("lemon");
+  });
+
+  //   .toBeCloseTo
+  test("has 0.3 cans in stock", () => {
+    expect(can.stockInStore + can.stockInWarehouse).toBeCloseTo(0.3);
+  });
+});
+
+function drinkAll(callback, flavour) {
+  if (flavour !== "octopus") {
+    callback(flavour);
+  }
+}
+
+// .toHaveBeenCalled
+describe("drinkAll", () => {
+  test("drink something lemon-flavoured", () => {
+    const drink = jest.fn();
+    drinkAll(drink, "lemon");
+    expect(drink).toHaveBeenCalled();
+  });
+
+  test("does not drink something octopus-flavoured", () => {
+    const drink = jest.fn();
+    drinkAll(drink, "octopus");
+    expect(drink).not.toHaveBeenCalled();
   });
 });
