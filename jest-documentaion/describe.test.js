d12f696cf8e11a008d3f051c705aee0f09a6ad88
wolfiee42 <saifalislam2022@gmail.com>
2024-10-30 08:49:54 +0600

    understanding the concept of toThrow error along with binaryString to number.

--- jest-documentaion/describe.test.js
+++ jest-documentaion/describe.test.js
@@ -11,3 +11,37 @@ describe("My beverage is ", () => {
     expect(myBeverage.sour).toBeFalsy(); //.toBeFalsy();
   });
 });
+
+//nested describe blocks, hierarchy of tests.
+
+class CustomError {
+  constructor() {
+    this.error = new Error();
+  }
+}
+
+const binaryStringToNumber = (binString) => {
+  if (!/^[01]+$/.test(binString)) {
+    throw new CustomError("Not a binary number.");
+  }
+
+  return parseInt(binString, 2);
+};
+
+describe("binary String To Number", () => {
+  describe("given an invalid binary string.", () => {
+    test("composed of non-numbers throws CustomErrors.", () => {
+      expect(() => binaryStringToNumber("abc")).toThrow(CustomError);
+    });
+
+    test("with extra white space throws CustomError", () => {
+      expect(() => binaryStringToNumber("   100")).toThrow(CustomError);
+    });
+  });
+
+  describe("given a valid number", () => {
+    test("returns the correct number.", () => {
+      expect(binaryStringToNumber("100")).toBe(4);
+    });
+  });
+});
