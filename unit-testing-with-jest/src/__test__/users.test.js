cf26dc194ed1cc2bd2a6b8977a4fe3a04a1c7bda
wolfiee42 <saifalislam2022@gmail.com>
2024-11-09 13:12:27 +0600

    test: creating test for get users when it returns 404

--- unit-testing-with-jest/src/__test__/users.test.js
+++ unit-testing-with-jest/src/__test__/users.test.js
@@ -17,4 +17,13 @@ describe("get users", () => {
     expect(mockResponse.send).toHaveBeenCalledWith(mockUsers[1]);
     expect(mockResponse.send).toHaveBeenCalledTimes(1);
   });
+
+  it("should return statusCode 404 when user is not found", () => {
+    const copyMockRequest = { ...mockRequest, findUserIndex: 100 };
+    getUserByIdHandler(copyMockRequest, mockResponse);
+    expect(mockResponse.sendStatus).toHaveBeenCalled();
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(404);
+    expect(mockResponse.sendStatus).toHaveBeenCalledTimes(1);
+    expect(mockResponse.send).not.toHaveBeenCalled();
+  });
 });
