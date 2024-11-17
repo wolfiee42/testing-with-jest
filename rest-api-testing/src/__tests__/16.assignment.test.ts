96407d21d28ddf2452e296d0632eee2d3c157857
wolfiee42 <saifalislam2022@gmail.com>
2024-11-17 16:32:07 +0600

    test: initialzing the test environment.

--- rest-api-testing/src/__tests__/16.assignment.test.ts
+++ rest-api-testing/src/__tests__/16.assignment.test.ts
@@ -1,15 +1,34 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
+
+
+const AuthService = {
+  login: jest.fn(),
+  getUserProfile: jest.fn(),
+  logout: jest.fn(),
+  checkLoginAttempt: jest.fn()
+}
+
+beforeAll(() => {
+  console.log("Initializing the test.")
+})
+
+afterAll(() => {
+  console.log('Clearing up the test environment.');
+})
+
 describe("Testing Auth Service", () => {
-  beforeAll(async () => {
-    const mongoServer = await MongoMemoryServer.create();
-    await mongoose.connect(mongoServer.getUri())
+
+  it('should return successfully login', async () => {
+
+    const username = 'saif42';
+    const password = 'holaAmigos42'
+
+    AuthService.login.mockReturnValueOnce({ username: 'saiif42', password: 'holaAmigos42' });
+    await AuthService.login(username, password);
+    expect(AuthService.login).toHaveBeenCalled();
+
   });
-
-  afterAll(async () => {
-    await mongoose.disconnect();
-    await mongoose.connection.close();
-  })
-
 
 });
