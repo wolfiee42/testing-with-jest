7c300832cddcabd48f0d4ca94df545c003e88a0d
wolfiee42 <saifalislam2022@gmail.com>
2024-11-18 17:11:29 +0600

    test: writing test on getting user profile.

--- rest-api-testing/src/__tests__/16.assignment.test.ts
+++ rest-api-testing/src/__tests__/16.assignment.test.ts
@@ -21,7 +21,7 @@ afterEach(() => {
   console.log('Test case completed.');
 });
 
-describe("Testing Auth Service", () => {
+describe("User Login Tests", () => {
 
   it("should login successfully with valid credentials", async () => {
     AuthService.login.mockResolvedValueOnce({ id: 1, name: "John Doe" });
@@ -43,3 +43,37 @@ describe("Testing Auth Service", () => {
     }
   });
 });
+
+
+describe('User Profile Test', () => {
+  it('should return full users profile at first attempt, and limited information at second.', async () => {
+    AuthService.getUserProfile.mockResolvedValueOnce({
+      id: 1,
+      name: 'saif',
+      email: 'saif@example.com',
+      password: "password123"
+    })
+      .mockResolvedValueOnce({
+        id: 1,
+        name: 'saif'
+      });
+
+    const getUserProfile1 = await AuthService.getUserProfile();
+    expect(getUserProfile1).toEqual({
+      id: 1,
+      name: 'saif',
+      email: 'saif@example.com',
+      password: "password123"
+    })
+
+
+    const getUserProfile2 = await AuthService.getUserProfile();
+    expect(getUserProfile2).toEqual({
+      id: 1,
+      name: 'saif'
+    })
+
+    expect(AuthService.getUserProfile).toHaveBeenCalledTimes(2);
+
+  });
+})
