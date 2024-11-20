e385a326a2f734b01081566a4f1aa92e182396fa
wolfiee42 <saifalislam2022@gmail.com>
2024-11-20 07:30:07 +0600

    test: learning superbase, testing user post and get errror.

--- rest-api-testing/src/__tests__/userManagement.test.ts
+++ rest-api-testing/src/__tests__/userManagement.test.ts
@@ -2,9 +2,24 @@ import request from 'supertest';
 import app from "../service/userManagement.service";
 
 describe('User Management API', () => {
+
     it('should return all users (GET, "/users") ', async () => {
         const response = await request(app).get('/users');
         expect(response.status).toBe(200);
         expect(response.body).toEqual([]); // Initially, no users
+    });
+
+    it('should create a new user (POST, "/users")', async () => {
+        const newUser = { name: 'Ashik', age: 20 };
+
+        const response = await request(app).post('/users').send(newUser);
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject({ id: 1, name: 'Ashik', age: 20 });
+    });
+
+    it('should return error (POST, "/users")', async () => {
+        const response = await request(app).post('/users').send({ name: 'Ashik' });
+        expect(response.status).toBe(400);
+        expect(response.body).toEqual({ error: 'Both name and message are required.' });
     });
 })
